18981ccb95b8585fb936542b3b869b9bb55f32b0
msntts <terzonome-github@yahoo.co.jp>
2022-10-19 01:31:11 +0900

    番号付き箇条書きを抽出するサンプルを追加

--- main.py
+++ main.py
@@ -7,7 +7,15 @@ if __name__ == '__main__':
     doc = pandoc.read(file='./sample/sample.md', format='markdown')
     pandoc.write(doc, './sample/sample.txt', format='plain')
 
-    for elm in pandoc.iter(doc):
+    contents = doc[1]
+
+    for elm in contents:
         # こんな感じで型を指定すればトップレベルの箇条書きは取れるはず・・
-        if isinstance(elm, OrderedList):
-            print(elm)
+        # OrderListの構文は OrderedList(ListAttributes, [[Block]])
+        # elm[1]で[[Block]](箇条書きとか本文のところ)を取得
+        index = 1
+        for block in elm[1]:
+            # OrderListの構造的にトップレベルの箇条書きタイトルは↓でアクセスできそう
+            # 箇条書きの番号は消える・・・レンダリングの時に付与してるのかな
+            print(f'{index}.{block[0][0][0][0]}')
+            index += 1
